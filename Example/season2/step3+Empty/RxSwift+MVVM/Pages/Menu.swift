e02cdeb6d90bd737516da6ecebf3527d94fe3d38
Jaehooooooon <jaehoon572@naver.com>
2020-04-15 15:31:46 +0900

    MenuListViewModel 생성

---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 서재훈 on 2020/04/15.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by 서재훈 on 2020/04/15.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+class MenuListViewModel {
+    
+    let menus: [Menu] = [
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김2", price: 100, count: 0),
+        Menu(name: "튀김3", price: 100, count: 0),
+        Menu(name: "튀김4", price: 100, count: 0)
+    ]
+}
